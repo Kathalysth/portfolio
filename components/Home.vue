43b27a784941a9a8f251baf2fdda99313650084d
Chris Ehigimetor <bishopsirhchris@gmail.com>
2022-09-11 16:36:23 +0100

    tab-movements

---
+++ components/Home.vue
@@ -0,0 +1,39 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="page homepage"
+    :class="{ 'active-page': isActive }"
+    v-on:enter="enter"
+    v-on:leave="leave"
+  >
+    <div class="content-wrapper">
+      <div class="details">
+        <div>
+          <h1>
+            <span class="title-name">I'M Chris Ehigimetor.</span>
+            <span>Frontend Developer</span>
+          </h1>
+          <p>
+            I'm a Javascript Developer that focuses on front-end frameworks such
+            as React, Vue, Nuxtjs and Nextjs. I have a passion for Design
+            Systems, Web Accessibility, Open Source Contribution, and System
+            Integration. To grow I focus on contributing to open source
+            projects.
+          </p>
+          <button>
+            More About me
+            <div class="btn-icon">_</div>
+          </button>
+        </div>
+      </div>
+      <div class="portrait"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: ["isActive"],
+};
+</script>
